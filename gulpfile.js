8d99a97aa3548426e9c3d3b13f2b80792bc62bad
Isabella Organ <isabellaorgan@gmail.com>
2015-11-09 07:57:18 -0800

    update dependencies, add gulpfile, work on test

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var testFiles = ['./test/**/*.js'];
+
+gulp.task('mocha', function() {
+	return gulp.src('test/*.js')
+	.pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('jshint:test', function() {
+	return gulp.src(testFiles)
+	.pipe(jshint({
+		node: true,
+		globals: {
+			describe: true,
+			it: true,
+			before: true,
+			after: true,
+		}
+
+	}))
+	.pipe(jshint.reporter('default'));
+});
+
+gulp.task('jshint', ['jshint:test']);
+gulp.task('default', ['jshint', 'mocha']);
--- test/test.js
+++ test/test.js
@@ -1,8 +1,44 @@
-var poparr = require('../lib/poparr.js');
-var pusharr = require('../lib/pusharr.js');
-var shiftarr = require('../lib/shiftarr.js');
-var unshiftarr = require('../lib/unshiftarr.js');
-var unique = require('../lib/unique.js');
-var freq_2 = require('../lib/frequency.js');
+var poparr = require(__dirname + '/../lib/poparr.js');
+var pusharr = require(__dirname + '/../lib/pusharr.js');
+var shiftarr = require(__dirname + '/../lib/shiftarr.js');
+var unshiftarr = require(__dirname + '/../lib/unshiftarr.js');
+var unique = require(__dirname + '/../lib/unique.js');
+var freq_2 = require(__dirname + '/../lib/freq_2.js');
 
 var expect = require('chai').expect;
+
+describe('pop function', function() {
+	it('should', function() {
+
+	});
+});
+
+describe('push function', function() {
+	it('should', function() {
+
+	});
+});
+
+describe('shift function', function() {
+	it('should', function() {
+
+	});
+});
+
+describe('unshift function', function() {
+	it('should', function() {
+
+	});
+});
+
+describe('unique function', function() {
+	it('should', function() {
+
+	});
+});
+
+describe('freq2 function', function() {
+	it('should', function() {
+
+	});
+});
