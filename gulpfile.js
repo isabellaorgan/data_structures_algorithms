97267183b5bbe93cb0ac1af70558a3d925c2e7fb
Isabella Organ <isabellaorgan@gmail.com>
2016-03-20 11:53:15 -0700

    adjust code style IDE set incorrectly during most of this course, due to incorrect instructions from TA - update tabs/spaces, etc accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@ var jshint = require('gulp-jshint');
 var testFiles = ['./test/**/*.js'];
 
 gulp.task('mocha', function() {
-	return gulp.src('test/*.js')
+  return gulp.src('test/*.js')
 	.pipe(mocha({reporter: 'spec'}));
 });
 
 gulp.task('jshint:test', function() {
-	return gulp.src(testFiles)
+  return gulp.src(testFiles)
 	.pipe(jshint({
-		node: true,
-		globals: {
-			describe: true,
-			it: true,
-			before: true,
-			after: true,
-		}
+  node: true,
+  globals: {
+    describe: true,
+    it: true,
+    before: true,
+    after: true,
+  }
 
 	}))
 	.pipe(jshint.reporter('default'));
